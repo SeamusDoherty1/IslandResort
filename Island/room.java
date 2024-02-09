93b44ed66b6ab447559b6dc069fbf37b60f996da
Seamus Doherty <bl4ster9000@gmail.com>
2024-02-09 12:58:36 -0500

    added hotels

---
+++ Island/room.java
@@ -0,0 +1,67 @@
+package Island;
+
+public class room{
+    private int tenants;
+    private boolean ac;
+    private boolean heating;
+    private double price;
+    private String type;
+    public room(){
+        tenants = 0;
+        ac = false;
+        heating = false;
+        price = 50.0;
+        type = "single";
+    }
+    public room(int te, boolean a, boolean h, double p, String t){
+        tenants = te;
+        ac = a;
+        heating = h;
+        price = p;
+        type = t;
+    }
+    public int getTenants(){
+        return tenants;
+    }
+    public boolean getAC(){
+        return ac;
+    }
+    public boolean getHeating(){
+        return heating;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getType(){
+        return type;
+    }
+    public void setTenants(int people){
+        if(people > 0){
+            tenants = people;
+        }
+        else{
+            tenants = 0;
+        }
+    }
+    public void setAC(boolean a){
+        ac = a;
+    }
+    public void setHeating(boolean h){
+        heating = h;
+    }
+    public void setPrice(double p){
+        price = p;
+    }
+    public void setType(String t){
+        type = t;
+    }
+    public boolean isEmpty(){
+        if(tenants == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
