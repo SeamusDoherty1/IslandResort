1d88c2416ed550516c4c60b2120269ec9158e863
Seamus Doherty <bl4ster9000@gmail.com>
2024-02-09 13:10:50 -0500

    updated hotel

--- Island/hotel.java
+++ Island/hotel.java
@@ -2,6 +2,25 @@ package Island;
 import java.util.*;
 
 public class hotel {
-    private ArrayList rooms = new ArrayList<room>();
-    private int profits
+    private ArrayList<room> rooms;
+    private double profits;
+    private int numRoom;
+    private int employees;
+    private int residents;
+
+    public hotel(){
+        rooms = new ArrayList<room>(); 
+        profits = 0;
+        numRoom = 0;
+        employees = 0;
+        residents = 0;
+    }
+    public hotel(room r, double p, int n, int e, int re){
+        rooms = new ArrayList<room>();
+        rooms.add(r);
+        profits = p;
+        numRoom = n;
+        employees = e;
+        residents = re;
+    }
 }
